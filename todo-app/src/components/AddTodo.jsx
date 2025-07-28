85ac826ebbdfff9973993b72d3bdc43504475567
mayurinambardar <71820138+mayurinambardar@users.noreply.github.com>
2025-07-28 10:01:19 +0000

    useRef hook

--- todo-app/src/components/AddTodo.jsx
+++ todo-app/src/components/AddTodo.jsx
@@ -1,42 +1,40 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { IoAdd } from "react-icons/io5";
 function AddTodo({onNewItem}) {
-  const [todoName,setTodoName]=useState('')
-  const [dueDate,setDueDate]=useState('')
-  const handleNameChange=(event)=>{
- setTodoName(event.target.value);
-  }
-   const handleDateChange=(event)=>{
- setDueDate(event.target.value);
-  }
-  const handleAddButtonClicked=()=>{
+const todoNameElement=useRef();
+const dueDateElement=useRef();
+  const handleAddButtonClicked=(e)=>{
+    e.preventDefault();
+    const todoName=todoNameElement.current.value;
+    const dueDate=dueDateElement.current.value;
+    todoNameElement.current.value=""
+    dueDateElement.current.value=""
     onNewItem(todoName,dueDate)
-    setDueDate("");
-    setTodoName("");
   }
   return (
     <>
-     <div className="flex flex-col md:flex-row items-center justify-center gap-4 p-4">
+     <form onSubmit={handleAddButtonClicked} className="flex flex-col md:flex-row items-center justify-center gap-4 p-4">
       <input
         type="text"
-        value={todoName}
+         ref={todoNameElement}  
         placeholder="Enter todo item"
-        onChange={handleNameChange}
         className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-      />
+    
+     required />
       <input
         type="date"
-        value={dueDate}
-        onChange={handleDateChange}
+        ref={dueDateElement}
         className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      required
       />
       <button
-        onClick={handleAddButtonClicked}
+        //  onClick={handleAddButtonClicked}
+        type="submit"
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
       >
         <IoAdd />
       </button>
-    </div>
+      </form>
     </>
   );
 }
--- todo-app/src/components/AppName.jsx
+++ todo-app/src/components/AppName.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import { FcTodoList } from "react-icons/fc";
 function AppName() {
   return (
-   <h1 className="text-3xl font-bold text-center py-4 bg-blue-100 text-blue-800 shadow-md">
-      Todo App
+    <>
+   <h1 className="text-3xl font-bold text-center py-4 bg-blue-100 text-blue-800 shadow-md flex items-center justify-center gap-2">
+      Todo App <FcTodoList className="text-4xl" />
     </h1>
+    
+    </>
   );
 }
 
